04d74180c7bb309e6da2d800896273a34e5b6698
SH-SharifAhamed <sharif845g7@gmail.com>
2026-05-19 00:21:09 +0600

    pat add form added

--- index.js
+++ index.js
@@ -1,7 +1,66 @@
-const express = require('express')
-const app = express()
+const dns = require("node:dns");
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
-const port = 5000
+
+
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const { log } = require("node:console");
+dotenv.config();
+
+const uri = process.env.MONGODB_URI;
+
+
+
+const app = express()
+const port = process.env.PORT
+
+
+app.use(cors())
+app.use(express.json())
+
+const client = new MongoClient(uri, {
+     serverApi: {
+          version: ServerApiVersion.v1,
+          strict: true,
+          deprecationErrors: true,
+     }
+});
+
+
+async function run() {
+     try {
+          await client.connect();
+
+
+          const database = client.db("petAdoption");
+          const petCollection = database.collection("pets");
+
+          app.post('/pets', async (req, res) => {
+               const petData = req.body;
+               console.log(petData);
+               
+               const result = await petCollection.insertOne(petData)
+
+               res.json(result)
+          })
+
+
+          await client.db("admin").command({ ping: 1 });
+          console.log("Pinged your deployment. You successfully connected to MongoDB!");
+     } finally {
+          // Ensures that the client will close when you finish/error
+          // await client.close();
+     }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+     res.send("server is running")
+})
 
 app.listen(port, () => {
      console.log(`Example app listening on port ${port}`)
